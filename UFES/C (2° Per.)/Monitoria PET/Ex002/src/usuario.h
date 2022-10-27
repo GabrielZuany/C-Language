0208aa7c2b329aaeb596deed4866ad92862ee271
GabrielZuany <gabrielzuanyduartevargas@gmail.com>
2022-10-26 23:31:29 -0300

    Src removed

--- UFES/C (2° Per.)/Monitoria PET/Ex002/src/usuario.h
+++
@@ -1,9 +0,0 @@
-typedef struct usuario Usuario;
-
-Usuario* CriaUsuario(char* nome, int cpf);
-
-char* RecuperaNomeUsuario(Usuario* usuario);
-
-int RecuperaCpfUsuario(Usuario* usuario);
-
-void DestroiUsuario(Usuario* usuario);
